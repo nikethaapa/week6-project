037330ef6af4783cb2856006ce3310676ced22a8
shyxmmmmmm <shyamkumar2005464@gmail.com>
2026-03-19 19:43:44 +0530

    added navbar and sidebar

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 var a=document.getElementById("one")
 var b=document.getElementById("two")
 var c=document.getElementById("three")
+var d=document.getElementById("four")
+var e=document.getElementById("five")
 
-a.addEventListener("click",function(){
+a.addEventListener("click",()=>{
     b.classList.toggle("translate-x-full")
 })
 
-c.addEventListener("click",function(){
+c.addEventListener("click",()=>{
     b.classList.toggle("translate-x-full")
 })
+
+d.addEventListener("click",()=>{
+    alert("Table Booked Successfully")
+})
+
+e.addEventListener("click",()=>{
+    alert("Item added to cart")
+})
+
+
