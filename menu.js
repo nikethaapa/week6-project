5d6b4330f59c9b42027b1b511c054dcfa85bf1d4
nikethaapa@gmail.com <nikethaapa@gmail.com>
2026-03-21 12:27:34 +0530

    Added menu page with search and filter

---
+++ menu.js
@@ -0,0 +1,39 @@
+const products = document.querySelectorAll(".product_item");
+const search = document.getElementById("search");
+const checkboxes = document.querySelectorAll('input[name="filter"]');
+
+// Run filter when typing
+search.addEventListener("keyup", filterProducts);
+
+// Run filter when checkbox changes
+checkboxes.forEach(box => {
+    box.addEventListener("change", filterProducts);
+});
+
+function filterProducts() {
+
+    const searchValue = search.value.toLowerCase();
+
+    const activeFilters = Array.from(checkboxes)
+        .filter(box => box.checked)
+        .map(box => box.value.toLowerCase());
+
+    products.forEach(product => {
+
+        const name = product.querySelector("h3").textContent.toLowerCase();
+        const tags = product.dataset.tags.toLowerCase();
+
+        const matchSearch = name.includes(searchValue);
+
+        const matchFilter =
+            activeFilters.length === 0 ||
+            activeFilters.some(f => tags.includes(f));
+
+        if (matchSearch && matchFilter) {
+            product.style.display = "block";
+        } else {
+            product.style.display = "none";
+        }
+
+    });
+}
