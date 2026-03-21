e2b52de06bcefc7a992b41f3a1335836277f21dc
AARTHI.S <aarthishanmugam701@gmail.com>
2026-03-21 20:26:58 +0530

    Initial commit

--- Js/script.js
+++ Js/script.js
@@ -20,4 +20,34 @@ e.addEventListener("click",()=>{
     alert("Item added to cart")
 })
 
+// Popular Dishes Filter
+const tabs = document.querySelectorAll(".tabs span");
+const cards = document.querySelectorAll(".card");
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        // Remove active class from all tabs
+        tabs.forEach(t => t.classList.remove("active"));
+        
+        // Add active class to clicked tab
+        tab.classList.add("active");
+        
+        // Get the filter type
+        const filterType = tab.textContent.toLowerCase();
+        
+        // Filter cards
+        cards.forEach(card => {
+            if (filterType === "all") {
+                card.style.display = "block";
+            } else {
+                if (card.classList.contains(filterType)) {
+                    card.style.display = "block";
+                } else {
+                    card.style.display = "none";
+                }
+            }
+        });
+    });
+});
+
 
