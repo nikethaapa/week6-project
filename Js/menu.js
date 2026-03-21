6a7f6cfdd43cc1e96ae22eef2488ffd737a5d614
shyxmmmmmm <shyamkumar2005464@gmail.com>
2026-03-21 21:21:37 +0530

    updated project with team changes

--- Js/menu.js
+++ Js/menu.js
@@ -37,3 +37,19 @@ function filterProducts() {
 
     });
 }
+
+
+
+
+var a=document.getElementById("one")
+var b=document.getElementById("two")
+var c=document.getElementById("three")
+
+
+a.addEventListener("click",()=>{
+    b.classList.toggle("translate-x-full")
+})
+
+c.addEventListener("click",()=>{
+    b.classList.toggle("translate-x-full")
+})
